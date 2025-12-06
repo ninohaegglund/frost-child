f4ed1c7085938d64977625529c1f1b4d334d38f3
Nino Hägglund <ninomh99@gmail.com>
2025-12-06 12:03:44 +0100

    remove transition effect

--- functions.php
+++ functions.php
@@ -4,9 +4,6 @@ add_action('wp_enqueue_scripts', function () {
   wp_enqueue_style('frost-parent', get_template_directory_uri() . '/style.css', [], wp_get_theme('frost')->get('Version'));
   wp_enqueue_style('frost-child', get_stylesheet_uri(), ['frost-parent'], wp_get_theme()->get('Version'));
 
-  wp_enqueue_style('page-transition', get_stylesheet_directory_uri() . '/assets/css/page-transition.css', [], '1.0');
-
-  wp_enqueue_script('page-transition', get_stylesheet_directory_uri() . '/assets/js/page-transition.js', [], '1.0', true);
 });
 
 add_filter('body_class', function ($classes) {
